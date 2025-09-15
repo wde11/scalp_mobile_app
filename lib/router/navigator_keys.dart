2e12ac1b9d1c09e86aaa308cbfae63f153c05ab3
w1 <tperalta_230000001926@uic.edu.ph>
2025-09-16 07:49:59 +0800

    Added Zoom feature
    zoom feature

--- lib/router/navigator_keys.dart
+++ lib/router/navigator_keys.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
 final GlobalKey<NavigatorState> shellNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> dashboardNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> listingNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> mapNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> chatNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> profileNavigatorKey = GlobalKey<NavigatorState>();
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final MapController _mapController = MapController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +18,53 @@ class MapScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Map'),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: const LatLng(7.0722, 125.6131),
-          initialZoom: 13.0,
-        ),
+      body: Stack(
         children: [
-          TileLayer(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: const ['a', 'b', 'c'],
+          FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter: const LatLng(7.0722, 125.6131),
+              initialZoom: 13.0,
+              interactionOptions: const InteractionOptions(
+                flags: InteractiveFlag.all,
+              ),
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: const ['a', 'b', 'c'],
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  heroTag: 'zoomIn',
+                  onPressed: () {
+                    _mapController.move(
+                      _mapController.camera.center,
+                      _mapController.camera.zoom + 1,
+                    );
+                  },
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(height: 8),
+                FloatingActionButton(
+                  heroTag: 'zoomOut',
+                  onPressed: () {
+                    _mapController.move(
+                      _mapController.camera.center,
+                      _mapController.camera.zoom - 1,
+                    );
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
           ),
         ],
       ),
