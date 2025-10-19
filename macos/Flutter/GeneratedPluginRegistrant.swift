8850d73b3514c3ece37f0741249f3786d7a3e108
wde11 <tperalta_230000001926@uic.edu.ph>
2025-10-19 19:08:38 +0800

    Add sqflite and update dependencies
    Added sqflite to pubspec.yaml and updated related plugin registration for macOS. Updated several dependencies to newer versions, including image_picker, firebase_core, firebase_auth, go_router, flutter_map, google_fonts, logger, path_provider, and flutter_lints. Synced generated plugin registrant files for macOS and Windows to include new and updated plugins.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,16 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< Updated upstream
 import file_selector_macos
-import path_provider_foundation
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-=======
 import firebase_auth
 import firebase_core
 import path_provider_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
->>>>>>> Stashed changes
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,20 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< Updated upstream
 #include <file_selector_windows/file_selector_windows.h>
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileSelectorWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorWindows"));
-=======
 #include <firebase_auth/firebase_auth_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
   FirebaseAuthPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseAuthPluginCApi"));
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
->>>>>>> Stashed changes
 }
